b21af0b397690885511583259c6a47c6e03ce36f
Ben Newman <benjamin.newman@uon.edu.au>
2019-04-04 00:22:42 +0000

    redo

---
+++ W5task1a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() 
+    {
+        int i;
+    int array[5];
+    
+    for (i=0;i<5;i++) 
+           {
+        printf("%d\n", i);
+        return 0;
+    }
+
+    }
